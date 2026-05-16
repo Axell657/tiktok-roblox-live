d98bcb57158abdfc3ee17732f5afc1ca128b205e
Axell657 <montivelaa@gmail.com>
2026-05-16 01:35:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,125 +2,131 @@ const { WebcastPushConnection } = require('tiktok-live-connector')
 const express = require('express')
 
 const app = express()
-
-const tiktokUsername = 'pochi_i.l0ve'
-
-const tiktokLiveConnection = new WebcastPushConnection(tiktokUsername)
+app.use(express.json())
 
 let latestData = ""
+let currentTikTokUsername = ""
+let tiktokLiveConnection = null
+
+function setLatest(username, gift, coins = 0) {
+    latestData = JSON.stringify({
+        username,
+        gift,
+        coins,
+        id: Date.now()
+    })
+}
+
+async function connectTikTok(username) {
+
+    currentTikTokUsername = username
+
+    if (tiktokLiveConnection) {
+        try {
+            tiktokLiveConnection.disconnect()
+        } catch(e){}
+    }
+
+    tiktokLiveConnection = new WebcastPushConnection(username)
+
+    tiktokLiveConnection.connect()
+    .then(state => {
+        console.log(`Conectado al live de ${state.uniqueId}`)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+    tiktokLiveConnection.on('chat', data => {
+
+        setLatest(
+            data.comment,
+            false,
+            0
+        )
+
+    })
+
+    tiktokLiveConnection.on('gift', data => {
+
+        if(data.comment){
+
+            setLatest(
+                data.comment,
+                true,
+                data.diamondCount || 1
+            )
+
+        }
+
+    })
+
+}
+
+app.get('/', (req,res)=>{
+
+    res.send("TikTok Roblox Server ON")
+
+})
 
 app.get('/latest', (req,res)=>{
+
     res.send(latestData)
+
+})
+
+app.get('/connect/:username', async (req,res)=>{
+
+    const username = req.params.username
+        .replace("@","")
+        .trim()
+
+    await connectTikTok(username)
+
+    res.send(`Conectando a ${username}`)
+
 })
 
 app.get('/test', (req,res)=>{
-    latestData = JSON.stringify({
-        username: "Builderman",
-        gift: false,
-        coins: 0,
-        id: Date.now()
-    })
+
+    setLatest(
+        "Builderman",
+        false,
+        0
+    )
+
     res.send("spawned")
+
 })
 
 app.get('/gift', (req,res)=>{
-    latestData = JSON.stringify({
-        username: "Roblox",
-        gift: true,
-        coins: 1,
-        id: Date.now()
-    })
+
+    setLatest(
+        "Roblox",
+        true,
+        1
+    )
+
     res.send("gift spawned")
+
 })
 
 app.get('/biggift', (req,res)=>{
-    latestData = JSON.stringify({
-        username: "Builderman",
-        gift: true,
-        coins: 50,
-        id: Date.now()
-    })
+
+    setLatest(
+        "Builderman",
+        true,
+        50
+    )
+
     res.send("big gift spawned")
+
 })
 
-app.get('/test2', (req,res)=>{
-    latestData = JSON.stringify({
-        username: "Roblox",
-        gift: false,
-        coins: 0,
-        id: Date.now()
-    })
-    res.send("spawned 2")
-})
+const PORT = process.env.PORT || 3000
 
-app.get('/test3', (req,res)=>{
-    latestData = JSON.stringify({
-        username: "Shedletsky",
-        gift: false,
-        coins: 0,
-        id: Date.now()
-    })
-    res.send("spawned 3")
-})
+app.listen(PORT, ()=>{
 
-app.get('/test4', (req,res)=>{
-    latestData = JSON.stringify({
-        username: "Telamon",
-        gift: false,
-        coins: 0,
-        id: Date.now()
-    })
-    res.send("spawned 4")
-})
-
-app.get('/test5', (req,res)=>{
-    latestData = JSON.stringify({
-        username: "Clockwork",
-        gift: false,
-        coins: 0,
-        id: Date.now()
-    })
-    res.send("spawned 5")
-})
-
-app.get('/test6', (req,res)=>{
-    latestData = JSON.stringify({
-        username: "Stickmasterluke",
-        gift: false,
-        coins: 0,
-        id: Date.now()
-    })
-    res.send("spawned 6")
-})
-
-app.listen(3000, ()=>{
     console.log("Servidor iniciado")
-})
 
-tiktokLiveConnection.connect()
-.then(state => {
-    console.log(`Conectado al live de ${state.uniqueId}`)
-})
-.catch(err => {
-    console.log(err)
-})
-
-tiktokLiveConnection.on('chat', data => {
-    latestData = JSON.stringify({
-        username: data.comment,
-        gift: false,
-        coins: 0,
-        id: Date.now()
-    })
-})
-
-tiktokLiveConnection.on('gift', data => {
-    if(data.comment){
-        latestData = JSON.stringify({
-            username: data.comment,
-            gift: true,
-            coins: data.diamondCount || 1,
-            id: Date.now()
-        })
-    }
 })
